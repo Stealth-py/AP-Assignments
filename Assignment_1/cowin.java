eddc6378f81cda8eca47ada367a62bb187b24706
Stealth-py <generalom1234@gmail.com>
2021-10-10 12:21:10 +0530

    minor additions

---
+++ Assignment_1/cowin.java
@@ -0,0 +1,14 @@
+package Assignment_1;
+import java.util.*;
+
+public class cowin{
+    public ArrayList<HashMap<String, ArrayList<Integer>>> vaccine;
+    
+    cowin(){
+        vaccine = new ArrayList<HashMap<String, ArrayList<Integer>>>();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
